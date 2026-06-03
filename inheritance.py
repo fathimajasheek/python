4f6f5d70e6132539e95e635984d9b95b91273ab7
fathimajasheek <fathimajasheek@gmail.com>
2026-06-03 13:04:57 +0530

    updated

--- inheritance.py
+++ inheritance.py
@@ -11,4 +11,21 @@ x=dog("jacky")
 x.sound()
 x.speak()
     
-    
+#multiple inheritance   
+class animal:
+    def __init__(self,name):
+        self.name=name
+    def sound(self):
+        print(f"{self.name} woofed")
+class human:
+    def __init__(self,name):
+        self.name=name
+    def sound1(self):
+        print(f"{self.name} smiled")
+class dog(animal,human):
+    def speak(self):
+        print(f"{self.name} cried")
+x=dog("jacky")
+x.sound()
+x.speak()
+x.sound1()
